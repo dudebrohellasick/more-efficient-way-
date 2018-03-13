1da99a337ea4933c57b7e0a9cffa7e2809ce0621
Brandon Ruiz <34893330+dudebrohellasick@users.noreply.github.com>
2018-03-13 00:05:58 -0700

    Update Thing.py

--- Thing.py
+++ Thing.py
@@ -2,7 +2,7 @@ with open('test.txt', 'w') as t:
 	
 	
 	one = raw_input('1: ')
-    two = raw_input("2: ")
+        two = raw_input("2: ")
 	three = raw_input("3: ")
 	
 	x = t.write("{} \n{} \n{}".format(one, two, three))
